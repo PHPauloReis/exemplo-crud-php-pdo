7538029d41d483c50be9959011d708f4d3f851a6
Paulo Reis <pauloreisprofissional@gmail.com>
2026-09-18 06:27:09 -0300

    Fix: Restore persistent connection option in PDO configuration and update HTTP request method in load test

--- k6/teste.js
+++ k6/teste.js
@@ -38,7 +38,7 @@ export default function () {
     quantidade: '10',
   };
 
-  const response = http.post('http://localhost:8080/grande.html', payload);
+  const response = http.get('http://localhost:8080/', payload);
 
   check(response, {
     'status HTTP é 200': (r) => r.status === 200,
